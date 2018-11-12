0cbd9a509d31df571689d782c2c0425568a7c1a2
Ahmad Hassoun <ahmad.hassoun@cgi.com>
2018-11-12 12:15:26 -0500

    create serverjs

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
-// TODO: create a basic server with express
-// that will send back the index.html file on a GET request to '/'
-// it should then send back jsonData on a GET to /data
+var express = require('express');
+var app = express();
+var jsonData = { count: 12, message: 'hey'};
 
-var jsonData = {count: 12, message: 'hey'};
+// redirect to home page
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html', function(err){
+        if(err){
+            res.status(500).send(err);
+        }
+    });
+});
+
+// Get data
+app.get('/data', function(req, res){
+    res.json(jsonData);
+});
+
+app.listen(3000);
